0a92341849fd3c21c2eb6f5de24f0cba24c98735
sharnajh <sharnajhossain@gmail.com>
2020-04-26 07:15:46 -0400

    css animations

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -3,7 +3,8 @@ import "./css/MenuItem.css";
 
 const MenuItem = ({ title, imgUrl, size }) => {
     return (
-        <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imgUrl})` }}>
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={{ backgroundImage: `url(${imgUrl})` }} />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">shop now</span>

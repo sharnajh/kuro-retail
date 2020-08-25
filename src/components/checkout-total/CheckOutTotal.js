367e1b22bebf370ac1af773a0f974e7b6e04cf33
Sharna Hossain <sharnajh@gmail.com>
2020-08-25 00:04:47 -0400

    added sticky element to checkout page

--- src/components/checkout-total/CheckOutTotal.js
+++ src/components/checkout-total/CheckOutTotal.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./css/CheckOutTotal.css";
 import StripeCheckOutButton from "../stripe-checkout-button/StripeCheckOutButton";
 import { connect } from "react-redux";
@@ -6,25 +6,48 @@ import { selectCartTotal } from "../../redux/cart/cart.selector";
 import AnimatedNumber from "react-animated-number";
 
 const CheckOutTotal = ({ cartItemsTotal }) => {
+  const [top, setTop] = useState(0);
+  const [width, setWidth] = useState(0);
+  const parent = useRef();
   const node = useRef();
+  const handleSticky = (e) => {
+    if (node.current) {
+      if (top - 90 <= window.scrollY) {
+        node.current.style.position = "fixed";
+        node.current.style.top = 0;
+        node.current.style.width = width + "px";
+      } else {
+        node.current.style.position = "relative";
+      }
+    }
+  };
   useEffect(() => {
-    console.log("current: ", node.current.getBoundingClientRect());
-  })
+    if (node.current && parent.current) {
+      setTop(node.current.getBoundingClientRect().top);
+      setWidth(parent.current.getBoundingClientRect().width);
+    }
+    window.addEventListener("scroll", handleSticky);
+    return () => {
+      window.removeEventListener("scroll", handleSticky);
+    };
+  }, [node, handleSticky]);
   return (
-    <div className="checkout-total" ref={node}>
-      <span className="total-price">
-        <span>Subtotal:</span>{" "}
-        <span>
-          $
-          <AnimatedNumber
-            initialValue={cartItemsTotal}
-            value={cartItemsTotal}
-            duration={300}
-            stepPrecision={0}
-          />
+    <div className="parent-wrapper" ref={parent}>
+      <div className="checkout-total" ref={node}>
+        <span className="total-price">
+          <span>Subtotal:</span>{" "}
+          <span>
+            $
+            <AnimatedNumber
+              initialValue={cartItemsTotal}
+              value={cartItemsTotal}
+              duration={300}
+              stepPrecision={0}
+            />
+          </span>
         </span>
-      </span>
-      <StripeCheckOutButton price={cartItemsTotal} />
+        <StripeCheckOutButton price={cartItemsTotal} />
+      </div>
     </div>
   );
 };
